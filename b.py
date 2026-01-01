c1a911e6a8b636e6b3da4b48ee51b103bbc63166
AsokTamang <ashoktmg2058@gmail.com>
2026-01-01 00:56:35 -0500

    writing file

--- b.py
+++ b.py
@@ -10,5 +10,6 @@ data = {'tom': {
         'phone': 123456
     }}
 
-with open(r'C:\Users\ashok\OneDrive\Desktop\change.txt', 'r') as f: #here we are writing in the file with w and here r means the string is raw string
-    print(f.read())
+with open(r'C:\Users\ashok\OneDrive\Desktop\change.txt', 'w') as f: #here we are writing in the file with w and here r means the string is raw string
+    (f.write(json.dumps(data)))
+
