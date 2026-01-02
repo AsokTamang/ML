7028b377093874f59418a34938c9819e824abf46
AsokTamang <ashoktmg2058@gmail.com>
2026-01-02 10:56:16 -0500

    adding the value in stock_price dict

--- a.py
+++ a.py
@@ -6,6 +6,18 @@ question = input('Enter your order: ')
 if question.lower() == 'print':
    for data in stock_price:
        print(f'{data} ==> {calculateaverage(stock_price[data])}')
+elif question.lower() == 'add':
+    stockname=input('Enter the name of your stock')
+    pricevalue = int(input('Enter the price of your stock'))
+    if stockname.lower() in stock_price:
+
+        stock_price[stockname].append(pricevalue)    #appending with the value from stock price
+        print(stock_price[stockname])
+    else:
+        stock_price[stockname]=[pricevalue]  #if the stock name doesnot exist then we just create new pair for this
+
+
+
 
 
 
