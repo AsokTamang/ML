f01e9bac9deb8a67bdd6cde420afc7042659738e
AsokTamang <ashoktmg2058@gmail.com>
2026-01-01 23:52:29 -0500

    few exercises

--- b.py
+++ b.py
@@ -15,7 +15,25 @@ for line in f:
     total=line.split(' ')  #here we are splitting the line from the given file at space
     print(len(total))   #as the line.split(' ') splits the line at space and gives us an array
     #its length gives us the total number of words
+f.close()
 
 
+with open(r'C:\Users\ashok\OneDrive\Desktop\change.txt', 'r') as f:  #while using with we donot need to close like above
+    for line in f:
+        print(line)
+
+
+
+def areaofshape(base,height,shape):
+    multiplication = base * height
+    if shape == "rectangle":
+        return multiplication
+    return (1/2) * multiplication
+
+
+def  print_pattern(n):
+    for i in range(1,n+1):
+        print('*' * i)
+print_pattern(4)
 
 
