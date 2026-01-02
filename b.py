d773ca769b81d17cdfd0d83d07d56f1c634fe731
AsokTamang <ashoktmg2058@gmail.com>
2026-01-01 23:39:50 -0500

    counting the total number of words

--- b.py
+++ b.py
@@ -10,7 +10,12 @@ data = {'tom': {
         'phone': 123456
     }}
 
-with open(r'C:\Users\ashok\OneDrive\Desktop\change.txt', 'r') as f: #here we are writing in the file with w and here r means the string is raw string
-    a=(f.read())
-    finaldata = json.loads(a)   #here we are converting the string available in the given file path into JSON object again
-print(finaldata)
+f= open(r'C:\Users\ashok\OneDrive\Desktop\change.txt', 'r')
+for line in f:
+    total=line.split(' ')  #here we are splitting the line from the given file at space
+    print(len(total))   #as the line.split(' ') splits the line at space and gives us an array
+    #its length gives us the total number of words
+
+
+
+
