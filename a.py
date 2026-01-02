e0ca88fe580dcbdadc8395dceb2b3faab9e96910
AsokTamang <ashoktmg2058@gmail.com>
2026-01-02 00:31:16 -0500

    dict algorithm

--- a.py
+++ a.py
@@ -1,1 +1,11 @@
-area = 'area is 100msqr.'
+def calculateaverage(lst):
+    return sum(lst)/len(lst)
+
+stock_price={'info':[600,630,620],'ril':[1430,1490,1567],'mtl':[234,180,160]}
+question = input('Enter your order: ')
+if question.lower() == 'print':
+   for data in stock_price:
+       print(f'{data} ==> {calculateaverage(stock_price[data])}')
+
+
+
