6115c2ea9903ef611f99368f504bec2e7c2d1b8c
AsokTamang <ashoktmg2058@gmail.com>
2026-01-05 13:10:38 -0500

    shpaes and size and sorting method using numpy

--- allaboutnumpy/nump.py
+++ allaboutnumpy/nump.py
@@ -13,7 +13,21 @@ endtime = time.time()
 print(endtime-start_time)
 
 rev=np.array([40,30,70])
-rev2=np.array([[40,30,70],[90,100,200]])
-print(rev.ndim)
+rev2=np.array([[40,30,70],[90,100,200]],dtype=np.float32)
+print(rev.ndim)  #number of dimensions
 print(rev2.ndim)  #here .ndim gives us the number of dimension of an array
 print(rev2[1,1])  #based on indexing, we are getting the number
+print(rev2.dtype)  #here dtype means the type of bytes this array is occupying
+print(rev2.size)  #here .size gives us the total number of elements in a array
+
+print(rev2.shape) #here .shape gives us the number of rows and columns from a given array
+a=(np.sort(rev2,axis=None))  #here we are sorting the 2 rowed array of matrix in a single array by using axis=None
+print(a)
+print(np.linspace(1,10,80))  #here we are generating 80 numbers of datas from 1 to 10 which are at an equal distance
+print(rev2.sum(axis=0))  #here axis = 0 gives us the sum of the numbers in a matrix in a vertical order
+print(rev2.sum(axis=1))  #here axis = 1 gives us the sum of the numbers in a matrix in a horizontal order
+
+
+print(rev2.itemsize)
+
+#here .itemsize gives us the amount of byte that each item or number in rev2 occupies
