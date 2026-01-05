8970b9aa8e779e5515ac71faf78ef1c2a754cd52
AsokTamang <ashoktmg2058@gmail.com>
2026-01-04 23:17:29 -0500

    creating dict of tuples

--- codes/exercisefile.py
+++ codes/exercisefile.py
@@ -5,3 +5,20 @@ with open("C:/Users/ashok/OneDrive/Desktop/customers.txt",'r') as f:
      data[name]=int(money)
 print(data)
 
+def calculate_rewards(total):
+    if 100<=total<=199:
+        return 'Bronze'
+    elif 200<=total<=499:
+        return 'Silver'
+    elif total>499:
+        return 'Gold'
+
+customers_summary = {}
+for key,value in data.items():
+    if 100<=value<=199:
+        customers_summary[key]=(value,'Bronze')
+    elif 200 <= value <= 499:
+        customers_summary[key]=(value,'Silver')
+    else:
+        customers_summary[key] = (value, 'Gold')
+print(customers_summary)
