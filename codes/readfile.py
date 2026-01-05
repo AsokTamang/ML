e73617b3069bb0e7c8d9fe88f34f67cdd6348607
AsokTamang <ashoktmg2058@gmail.com>
2026-01-04 21:12:40 -0500

    writing in the file

--- codes/readfile.py
+++ codes/readfile.py
@@ -1,2 +1,4 @@
 with open("C:/Users/ashok/OneDrive/Desktop/change.txt",'r') as f:
      print(f.readlines())  #reading multiple lines
+with open ("C:/Users/ashok/OneDrive/Desktop/expenses.txt",'w') as f:
+     f.writelines(['I love to code\n','I love python and javascript']) #here \n is for line separator
