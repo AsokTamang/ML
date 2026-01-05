8bf14ca18258f72c121fcd2398c1ed13b5b4caa9
AsokTamang <ashoktmg2058@gmail.com>
2026-01-04 19:20:32 -0500

    stock management system

--- a.py
+++ a.py
@@ -122,3 +122,15 @@ print(scores[:3]) #score of first 3 students
 print(scores[2:5]) #score of roll 3 , 4 and 5
 scores.append(83)
 print(scores)
+
+
+
+#stock management system
+product_names = ["Apples", "Bananas", "Oranges", "Pears", "Grapes"]
+stock_levels = [20, 50, 15, 5, 8]
+minimum_stock = 10
+reorder_list = []
+for product,stock in zip(product_names,stock_levels):
+    if stock < minimum_stock:
+        reorder_list.append(product)
+print(reorder_list)
