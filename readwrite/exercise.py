2a5b0fe0436d0eeeb28f739fd38b9b66d9ea0c45
AsokTamang <ashoktmg2058@gmail.com>
2026-01-06 18:14:58 -0500

    assiging new column data based on the prev data

--- readwrite/exercise.py
+++ readwrite/exercise.py
@@ -1,3 +1,6 @@
-import pandas as pd 
+import pandas as pd
+
 df = pd.read_csv("C:/Users/ashok/Downloads/64101194a2364/source-code/2_pandas_basics_code/3_read_write_to_excel/Exercises/movies_data.csv")
-print(df.head(5))
+df['year_classify']=df['release_year'].apply(lambda x:'before 2000' if x<2000 else 'after 2000')  #here based on the column release_year, we are designing a new column data
+#year_classify
+print(df)
