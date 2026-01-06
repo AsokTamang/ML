f093eca4fd0df94a64613d143b19d8f3815a3b3c
AsokTamang <ashoktmg2058@gmail.com>
2026-01-06 18:23:38 -0500

    creating a new csv file using df.to_csv

--- readwrite/exercise.py
+++ readwrite/exercise.py
@@ -4,3 +4,6 @@ df = pd.read_csv("C:/Users/ashok/Downloads/64101194a2364/source-code/2_pandas_ba
 df['year_classify']=df['release_year'].apply(lambda x:'before 2000' if x<2000 else 'after 2000')  #here based on the column release_year, we are designing a new column data
 #year_classify
 print(df)
+df_new = df[['movie_id','title','budget','revenue','year_classify']]
+print(df_new)
+df_new.to_csv('C:/pythonprojects/final_movies.csv',index=False)  #here we are ignoring the indices
