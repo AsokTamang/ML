b98d1ecdf403a8169216b879a6744077e36f4f71
AsokTamang <ashoktmg2058@gmail.com>
2026-01-06 13:03:05 -0500

    getting name of employee having highest id

--- allaboutnumpy/pandass.py
+++ allaboutnumpy/pandass.py
@@ -5,8 +5,17 @@ df=pd.read_excel("C:/pythonprojects/start.csv.xlsx")
 print(df.head(3))
 print(df.shape)  #so our excel file consists of 10 rows and 8 columns
 maximum_salary = df.salary.max()  #here we are getting the maximum value of the salary
-print(df[df['salary']==maximum_salary]['name'])  #here we are getting the name of the staff having maximum salary
+print(df[df['salary']==maximum_salary].name)  #here we are getting the name of the staff having maximum salary using the mask method
+#which prints alex
 print(df.columns.astype(str))
 print(df.department.unique())  #printing the unique departments
 #counting the total number of unique department and conerting the returned data into dictionary
 print(df['department'].value_counts().rename_axis(None).rename(None).to_dict())
+
+
+
+df_new = df[['employee_id','name','department']]  #filtering the columns
+print(df_new)
+print(df_new[df_new['employee_id'] == df_new.employee_id.max()].name.rename(None))  #here .rename(None) removes the series name
+
+
