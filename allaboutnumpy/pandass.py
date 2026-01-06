6cad34b5d76dad6b8284d03aad251d8350ec823c
AsokTamang <ashoktmg2058@gmail.com>
2026-01-06 12:54:31 -0500

    counting the number of unique departments

--- allaboutnumpy/pandass.py
+++ allaboutnumpy/pandass.py
@@ -8,3 +8,5 @@ maximum_salary = df.salary.max()  #here we are getting the maximum value of the 
 print(df[df['salary']==maximum_salary]['name'])  #here we are getting the name of the staff having maximum salary
 print(df.columns.astype(str))
 print(df.department.unique())  #printing the unique departments
+#counting the total number of unique department and conerting the returned data into dictionary
+print(df['department'].value_counts().rename_axis(None).rename(None).to_dict())
