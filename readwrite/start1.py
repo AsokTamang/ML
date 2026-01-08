250507706e9a1eba7ca779ce22fd66ff4863a65b
AsokTamang <ashoktmg2058@gmail.com>
2026-01-08 13:05:40 -0500

    removing the outliers

--- readwrite/start1.py
+++ readwrite/start1.py
@@ -22,4 +22,21 @@ def converter(value):
 df_bookings['no_guests'].fillna(0,inplace=True)    #here first of all we are filling the null numbers with 0 inplace the current dataframe
 df_bookings['no_guests']=df_bookings['no_guests'].apply(lambda x:converter(x))  #here we are converting the column no_guests data using the function converter
 print(df_bookings)
+df_agg_bookings.groupby('property_id')['successful_bookings'].value_counts()  #here we are grouping by the property id and counting the total successful
+df_agg_bookings.groupby(['property_id', 'successful_bookings']).size()
+
+df_bookings.describe()
+print(df_bookings[df_bookings['no_guests']<=0])  #here we are doing the logical condition on the rows of df_bookings dataframe in the column no_guests
+df_bookings['no_guests'].fillna(0,inplace=True)    #here first of all we are filling the null numbers with 0 inplace the current dataframe
+df_bookings.loc[df_bookings['no_guests']<=0,'no_guests']=0 #here we are changing the column value of no_guests based on the row value of no_guests
+df_bookings['revenue_generated'].max()
+avg,std=df_bookings['revenue_generated'].mean(),df_bookings['revenue_generated'].std()  #here we are calculating the mean and standard deviation of the column revenue_generated
+print(avg,std)
+limit=avg + 3*std
+
+#removing the outliers
+df_bookings= df_bookings[df_bookings['revenue_generated']<=limit]     #here we are removing the df data whose revenue_generated column data has value more than the limit value,
+df_bookings['revenue_generated'].max()
+print(df_bookings)
+#bookings
 
