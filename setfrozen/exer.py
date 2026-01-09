8cbc9af52640724128e696f2675bb2ed0351741c
AsokTamang <ashoktmg2058@gmail.com>
2026-01-09 13:03:21 -0500

    using set for basic  mathematical calculation

---
+++ setfrozen/exer.py
@@ -0,0 +1,6 @@
+a= {'jack','rose','mike','lucy'}
+b = {'adam','brian','nikki','sam','jack','lucy'}
+print(a | b)  #total name in both sets a and b
+print(a & b)  #common name
+print(a-b)  #unique data of a in the combined set of a and b
+print(b-a)  #unique data of b in the combined set of a and b
